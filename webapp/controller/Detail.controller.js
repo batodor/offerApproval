d0411230e88e170e708ed19abc6ed984a932c367
SUEK SUEK <bit@suek.ru>
2018-10-05 07:57:59 +0000

    fix

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -504,24 +504,24 @@ sap.ui.define([
 				window.open("/sap/bc/ui2/flp#ZTS_OFFER-change&/" + this.TCNumber);
 			},
 			
-			copy: function(oEvent){
-				var oFuncParams = { 
-					OfferNumber: this.TCNumber
-				};
-				this.getModel().callFunction("/CopyOfferToTradingContract", {
-					method: "POST",
-					urlParameters: oFuncParams,
-					success: this.onCopySuccess.bind(this, "CopyOfferToTradingContract")
-				});
-			},
-			onCopySuccess: function(link, oData) {
-				var oResult = oData[link];
-				if (oResult.CopyingSuccessfully) {
-					MessageToast.show(oResult.Message);
-				} else {
-					MessageBox.error(oResult.Message);
-				}
-			},
+			// copy: function(oEvent){
+			// 	var oFuncParams = { 
+			// 		OfferNumber: this.TCNumber
+			// 	};
+			// 	this.getModel().callFunction("/CopyOfferToTradingContract", {
+			// 		method: "POST",
+			// 		urlParameters: oFuncParams,
+			// 		success: this.onCopySuccess.bind(this, "CopyOfferToTradingContract")
+			// 	});
+			// },
+			// onCopySuccess: function(link, oData) {
+			// 	var oResult = oData[link];
+			// 	if (oResult.CopyingSuccessfully) {
+			// 		MessageToast.show(oResult.Message);
+			// 	} else {
+			// 		MessageBox.error(oResult.Message);
+			// 	}
+			// },
 			
 			// Set odata from any dialog, argument object = any object / return object inputs Data
 			getData: function(object, isSave){
